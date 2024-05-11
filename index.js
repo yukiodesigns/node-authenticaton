f4e44473c27e3abce7ca0039dd4ac214b74a6995
yuki designs <yukiosdesigns@gmail.com>
2024-05-11 17:10:53 +0300

    finished and tested

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const  mongoose = require('mongoose')
-const PORT = 3000 || process.env.PORT
-const {MONGO_URI} = require('./config')
+const PORT = 5000 || process.env.PORT
+const {mongoURI} = require('./config')
 const user = require('./routes/users')
 const auth = require('./routes/auth')
 const app = express()
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use('/api/auth', auth)
 app.use('/api/user', user)
 
-mongoose.connect(MONGO_URI).then(()=> console.log('Mongo is connected'))
+mongoose.connect(mongoURI).then(()=> console.log('Mongo is connected'))
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
