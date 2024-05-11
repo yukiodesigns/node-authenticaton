18258128b0ee200e27b849638cc9fcec3c2c955c
yuki designs <yukiosdesigns@gmail.com>
2024-05-11 17:01:43 +0300

    Created config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  port: 3000,
+  mongoURI: 'mongodb+srv://yukiosdesigns:ilovekawira@cluster0.3b4nvmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  secret: 'yukiisavibe'
+}
